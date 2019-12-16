2e527ea3046eecd7afb0904fa38c7e48bdbb2040
Denis Dovgan <32987526+CsgoAtlas@users.noreply.github.com>
2019-12-16 17:22:53 +0200

    Add files via upload

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>;
+#include <string>;
+using namespace std;
+
+int main()
+{
+	int num;
+	int perc;
+	string result;
+	string birb = " ворона";
+	string birb1 = " вороны";
+	string birb2 = " ворон";
+	cout << "¬ведите свое число:" << endl;
+	cin >> num;
+	if (num == 1)
+	{
+		result = cout << num << birb;
+	};
+	if else (num > 1 & num < 5)
+	{
+		result = num + birb1;
+	}
+	else
+	{
+		result = num + birb1;
+	}
+	return result;
+}
